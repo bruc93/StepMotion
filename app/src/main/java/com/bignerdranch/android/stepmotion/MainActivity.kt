8e0b31988fcae5a2b9d7ddfae89a415e6f1e12c1
bruc93 <alexander.bruce93@hotmail.com>
2020-08-13 15:39:38 +0200

    Implementation fragments

--- app/src/main/java/com/bignerdranch/android/stepmotion/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/stepmotion/MainActivity.kt
@@ -13,17 +13,12 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_main.view.*
 
 
-class MainActivity : AppCompatActivity(), SensorEventListener {
-
-    private var sensorManager: SensorManager? = null
-
+class MainActivity : AppCompatActivity() {
+    //private val viewModel2 by viewModels<StepMotionViewModel>()
     private lateinit var viewModel:StepMotionViewModel
-
-    private lateinit var textStepCount:TextView
-    private lateinit var textKcalBurnt:TextView
-    private lateinit var textDistanceWalked:TextView
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,87 +27,40 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
         viewModel = ViewModelProvider(this).get(StepMotionViewModel::class.java)
 
-        textStepCount = findViewById(R.id.step_count)
-        textKcalBurnt = findViewById(R.id.kcal_text)
-        textDistanceWalked = findViewById(R.id.km_text)
-
-        //textDistanceWalked.text = viewModel.getDistanceWalkedKM().toString() + " KM"
-        textDistanceWalked.text = String.format("%.2f", viewModel.getDistanceWalkedKM()) + " KM"
-        textKcalBurnt.text = viewModel.getKcalBurnt().toInt().toString() + " KCAL"
-        textStepCount.text = viewModel.getTotalStep().toInt().toString();
-
         loadData()
 
-        resetSteps()
+        val mainFragment = MainFragment()
+        val meFragment = MeFragment()
 
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-
-    }
-
-    override fun onResume() {
-        super.onResume()
-        viewModel.setRunning(true)
-
-        val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
-
-        if(stepSensor == null){
-            Toast.makeText(this, "No sensor detected on this device", Toast.LENGTH_LONG)
-
-        }else{
-            sensorManager?.registerListener(this,stepSensor,SensorManager.SENSOR_DELAY_UI)
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, mainFragment)
+            commit()
         }
-    }
 
-
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-    }
-
-    @SuppressLint("SetTextI18n")
-    override fun onSensorChanged(event: SensorEvent) {
-        if(viewModel.getIfRunning())
-        {
-            viewModel.setTotalStep(event.values[0])
-
-            textStepCount.text =  viewModel.getCurrentStep().toInt().toString()
-            textKcalBurnt.text = viewModel.getKcalBurnt().toInt().toString() + " KCAL"
-            textDistanceWalked.text = String.format("%.2f", viewModel.getDistanceWalkedKM()) + " KM"
-            progress_circular.apply {
-                setProgressWithAnimation(viewModel.getCurrentStep())
+        buttonMe.setOnClickListener{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, meFragment)
+                commit()
             }
         }
-    }
 
-    @SuppressLint("SetTextI18n")
-    fun resetSteps(){
-        textStepCount.setOnClickListener{
-            Toast.makeText(this, "Long tap to reset steps", Toast.LENGTH_SHORT).show()
-        }
-        textStepCount.setOnLongClickListener{
-            viewModel.setPreviousTotalSteps(viewModel.getTotalStep())
-            textStepCount.text = 0.toString()
-            textKcalBurnt.text = 0.toString() + " KCAL"
-            textDistanceWalked.text = 0.toString() + " KM"
-            saveData()
-
-            progress_circular.apply {
-                setProgressWithAnimation(0f)
+        buttonHealth.setOnClickListener{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, mainFragment)
+                commit()
             }
-
-            true
         }
+
+
     }
 
-    private fun saveData(){
+    private fun loadData()
+    {
         val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-
-        editor.putFloat("key1", viewModel.getPreviousTotalSteps())
-        editor.apply()
-    }
-    private fun loadData(){
-        val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
-        val savedNumber = sharedPreferences.getFloat("key1", 0f)
+        val savedNumber = sharedPreferences?.getFloat("key1", 0f)
         Log.d("MainActivity", "$savedNumber")
-        viewModel.setPreviousTotalSteps(savedNumber)
+        if (savedNumber != null) {
+            viewModel.setPreviousTotalSteps(savedNumber)
+        }
     }
 }
--- app/src/main/java/com/bignerdranch/android/stepmotion/MainFragment.kt
+++ app/src/main/java/com/bignerdranch/android/stepmotion/MainFragment.kt
@@ -1,10 +1,23 @@
 package com.bignerdranch.android.stepmotion
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Context.SENSOR_SERVICE
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.fragment_main.view.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,7 +29,12 @@ private const val ARG_PARAM2 = "param2"
  * Use the [MainFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), SensorEventListener {
+    private var sensorManager: SensorManager? = null
+    private lateinit var viewModel:StepMotionViewModel
+    private lateinit var textStepCount: TextView
+    private lateinit var textKcalBurnt: TextView
+    private lateinit var textDistanceWalked: TextView
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -27,14 +45,61 @@ class MainFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        sensorManager = activity!!.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        viewModel = ViewModelProvider(requireActivity()).get(StepMotionViewModel::class.java)
+
+        Log.d("fragment", "We CREATE the fragment!!!!")
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+
+        var view = inflater.inflate(R.layout.fragment_main, container, false)
+
+        textStepCount = view.findViewById(R.id.step_count)
+        textKcalBurnt = view.findViewById(R.id.kcal_text)
+        textDistanceWalked = view.findViewById(R.id.km_text)
+
+
+        var stepeer = viewModel.getCurrentStep()
+        var stepeer2 = viewModel.getPreviousTotalSteps()
+        Log.d("getCurrentStep2", "$stepeer")
+        Log.d("getpreviusTotalStepp", "$stepeer2")
+        Log.d("fragment", "We createView the fragment!!!!")
+        textDistanceWalked.text = String.format("%.2f", viewModel.getDistanceWalkedKM()) + " KM"
+        textKcalBurnt.text = viewModel.getKcalBurnt().toInt().toString() + " KCAL"
+        textStepCount.text = viewModel.getCurrentStep().toInt().toString();
+
+        textStepCount.setOnClickListener{
+            Toast.makeText(this.activity, "Long tap to reset steps", Toast.LENGTH_SHORT).show()
+        }
+        textStepCount.setOnLongClickListener{
+            resetSteps()
+            true
+        }
+
+        return view
+    }
+
+    @SuppressLint("ShowToast")
+    override fun onResume() {
+        super.onResume()
+        Log.d("fragment", "We resumed the fragment!!!!")
+
+        viewModel.setRunning(true)
+
+        val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+        if(stepSensor == null){
+            Toast.makeText(this.activity, "No sensor detected on this device", Toast.LENGTH_LONG)
+
+        }else{
+            sensorManager?.registerListener(this,stepSensor,SensorManager.SENSOR_DELAY_UI)
+        }
     }
 
     companion object {
@@ -56,4 +121,49 @@ class MainFragment : Fragment() {
                 }
             }
     }
+
+    @SuppressLint("SetTextI18n")
+    public fun resetSteps(){
+        viewModel.setPreviousTotalSteps(viewModel.getTotalStep())
+        textStepCount.text = 0.toString()
+        textKcalBurnt.text = 0.toString() + " KCAL"
+        textDistanceWalked.text = 0.toString() + " KM"
+
+        saveData()
+
+        this.view?.progress_circular?.apply {
+            setProgressWithAnimation(0f)
+        }
+    }
+
+    private fun saveData(){
+        val sharedPreferences = this.activity?.getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+        val editor = sharedPreferences?.edit()
+
+        editor?.putFloat("key1", viewModel.getPreviousTotalSteps())
+        editor?.apply()
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onSensorChanged(event: SensorEvent?) {
+
+        var stepeer = viewModel.getCurrentStep().toInt();
+        Log.d("getCurrentStep", "$stepeer")
+        if(viewModel.getIfRunning())
+        {
+            viewModel.setTotalStep(event!!.values[0])
+        }
+        textStepCount.text = viewModel.getCurrentStep().toInt().toString()
+        textKcalBurnt.text = viewModel.getKcalBurnt().toInt().toString() + " KCAL"
+        textDistanceWalked.text = String.format("%.2f", viewModel.getDistanceWalkedKM()) + " KM"
+        this.view?.progress_circular?.apply {
+            setProgressWithAnimation(viewModel.getCurrentStep())
+        }
+
+
+    }
+
 }
